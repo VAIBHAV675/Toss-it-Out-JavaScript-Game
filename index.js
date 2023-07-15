8c97ffc1ba20c28f3d44440804e7ad92bc60c942
VAIBHAV SHARMA <85764094+VAIBHAV675@users.noreply.github.com>
2023-07-15 20:02:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+let name1=prompt("Enter the name of the first person");
+name1=name1.toUpperCase();
+document.getElementById("p1").innerHTML = name1;
+let name2=prompt("Enter the name of the second person");
+name2=name2.toUpperCase();
+document.getElementById("p2").innerHTML=name2;
+let randomperson=Math.floor((Math.random()*2)+1);
+let tossperson;
+if(randomperson===1)
+{
+    tossperson=name1;
+}
+else
+{
+    tossperson=name2;
+}
+let choice=prompt("What's your call "+tossperson+" : type (heads) or (tails) OR (h) or (t)");
+choice=choice.toUpperCase();
+let randomnumber=Math.floor(Math.random()*2)+1;
+if(randomnumber===1 && tossperson===name1)
+{
+    document.querySelectorAll("img")[0].setAttribute("src","./images/head.jpeg");
+    document.querySelectorAll("img")[1].setAttribute("src","./images/tail.jpeg");
+    if(choice==="HEADS" || choice==="H")
+    {
+        document.querySelector("h1").innerHTML="🚩 "+name1+" wins";
+    }
+    else
+    {
+        document.querySelector("h1").innerHTML=name2+" wins 🚩";
+    }
+}
+else if(randomnumber===2 && tossperson===name1)
+{
+    document.querySelectorAll("img")[0].setAttribute("src","./images/tail.jpeg");
+    document.querySelectorAll("img")[1].setAttribute("src","./images/head.jpeg");
+    if(choice==="TAILS" || choice==="T")
+    {
+        document.querySelector("h1").innerHTML="🚩 "+name1+" wins";
+    }
+    else
+    {
+        document.querySelector("h1").innerHTML=name2+" wins 🚩";
+    }
+}
+else if(randomnumber===1 && tossperson===name2)
+{
+    document.querySelectorAll("img")[0].setAttribute("src","./images/head.jpeg");
+    document.querySelectorAll("img")[1].setAttribute("src","./images/tail.jpeg");
+    if(choice==="HEADS" || choice==="H")
+    {
+        document.querySelector("h1").innerHTML="🚩 "+name1+" wins";
+    }
+    else
+    {
+        document.querySelector("h1").innerHTML=name2+" wins 🚩";
+    }
+}
+else if(randomnumber===2 && tossperson===name2)
+{
+    document.querySelectorAll("img")[0].setAttribute("src","./images/tail.jpeg");
+    document.querySelectorAll("img")[1].setAttribute("src","./images/head.jpeg");
+    if(choice==="TAILS" || choice==="T")
+    {
+        document.querySelector("h1").innerHTML="🚩 "+name1+" wins";
+    }
+    else
+    {
+        document.querySelector("h1").innerHTML=name2+" wins 🚩";
+    }
+}
+    
